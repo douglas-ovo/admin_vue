017711c2e0241e051ec581117c7d45205cf39649
liutao <lessthan4408@163.com>
2022-06-13 15:32:39 +0800

    charts.json

--- packages/mock/source/api.ts
+++ packages/mock/source/api.ts
@@ -15,6 +15,75 @@ const api: MockMethod[] = [
         }
     },
     {
+        url: '/charts.json',
+        method: 'get',
+        response(option: any) {
+            return {
+                chart1: {
+                    title: {
+                        text: '商品销量'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            name: '销量',
+                            type: 'bar',
+                            data: [
+                                Mock.mock({
+                                    "number|1-100": 100
+                                }).number, Mock.mock({
+                                    "number|1-100": 100
+                                }).number, Mock.mock({
+                                    "number|1-100": 100
+                                }).number, Mock.mock({
+                                    "number|1-100": 100
+                                }).number, Mock.mock({
+                                    "number|1-100": 100
+                                }).number, Mock.mock({
+                                    "number|1-100": 100
+                                }).number
+                            ]
+                        }
+                    ]
+                },
+                chart2: {
+                    title: {
+                        text: '分类销量'
+                    },
+                    series: [
+                        {
+                            type: 'pie',
+                            data: [
+                                {
+                                    value: Mock.mock({
+                                        "number|1-100": 1000
+                                    }).number,
+                                    name: '直接访问'
+                                },
+                                {
+                                    value: Mock.mock({
+                                        "number|1-100": 1000
+                                    }).number,
+                                    name: '联盟广告'
+                                },
+                                {
+                                    value: Mock.mock({
+                                        "number|1-100": 1000
+                                    }).number,
+                                    name: '搜索引擎'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    {
         url: '/userinfo.json',
         method: 'get',
         response() {
--- packages/renderer/src/views/home/Charts.vue
+++ packages/renderer/src/views/home/Charts.vue
@@ -14,51 +14,16 @@ export default {
 <script lang="ts" setup>
 import { ref, reactive, onMounted } from 'vue'
 import * as echarts from 'echarts';
+import axios from '../../http';
+
 const container1 = ref<HTMLElement | null>(null)
 const container2 = ref<HTMLElement | null>(null)
 const initChart = () => {
-    const myChart1 = echarts.init(container1.value as HTMLElement)
-    myChart1.setOption({
-        title: {
-            text: '商品销量'
-        },
-        tooltip: {},
-        xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [
-            {
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }
-        ]
-    });
-    const myChart2 = echarts.init(container2.value as HTMLElement)
-    myChart2.setOption({
-        title: {
-            text: '分类销量'
-        },
-        series: [
-            {
-                type: 'pie',
-                data: [
-                    {
-                        value: 335,
-                        name: '直接访问'
-                    },
-                    {
-                        value: 234,
-                        name: '联盟广告'
-                    },
-                    {
-                        value: 1548,
-                        name: '搜索引擎'
-                    }
-                ]
-            }
-        ]
+    axios.get('/charts.json', { params: {} }).then(res => {
+        const myChart1 = echarts.init(container1.value as HTMLElement)
+        myChart1.setOption(res.data.chart1);
+        const myChart2 = echarts.init(container2.value as HTMLElement)
+        myChart2.setOption(res.data.chart2)
     })
 }
 
