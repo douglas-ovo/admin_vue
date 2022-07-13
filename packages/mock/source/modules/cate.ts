71b16a928e2e45a8479a47e5f1a46adf881bfaab
liutao <lessthan4408@163.com>
2022-07-13 16:59:35 +0800

    商品管理

--- packages/mock/source/modules/cate.ts
+++ packages/mock/source/modules/cate.ts
@@ -40,17 +40,6 @@ let cate: IPubilc[] = [
         setTime: Random.now('week'),
     },
 ]
-
-let goods: IPubilc[] = [
-    {
-        id: Mock.mock('@id()'),
-        name: '手机',
-        status: false,
-        setTime: Random.now('week'),
-        cate: 1
-    },
-]
-
 const pageCate = (page: any, pageSize: any) => {
     let proportion = pageSize;
     let num = 0;
@@ -67,7 +56,63 @@ const pageCate = (page: any, pageSize: any) => {
     return _data[page - 1];
 }
 
+
+let goods: IPubilc[] = [
+    {
+        id: Mock.mock('@id()'),
+        name: 'iPhone14',
+        setTime: Random.now('week'),
+        cate: 1
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: '小米粥',
+        setTime: Random.now('week'),
+        cate: 2
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: '公牛插座',
+        setTime: Random.now('week'),
+        cate: 3
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: '短裤男夏季潮流',
+        setTime: Random.now('week'),
+        cate: 4
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: '碎花裙女夏季潮流',
+        setTime: Random.now('week'),
+        cate: 5
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: '儿童速干衣',
+        setTime: Random.now('week'),
+        cate: 6
+    },
+]
+const pageGoods = (page: any, pageSize: any) => {
+    let proportion = pageSize;
+    let num = 0;
+    let _data = [];
+    for (let i = 0; i < goods.length; i++) {
+        if (i % proportion == 0 && i != 0) {
+            _data.push(goods.slice(num, i));
+            num = i;
+        }
+        if ((i + 1) == goods.length) {
+            _data.push(goods.slice(num, (i + 1)));
+        }
+    }
+    return _data[page - 1];
+}
+
 export default [
+    //分类管理
     {
         url: "/getcate.json",
         method: 'get',
@@ -108,10 +153,10 @@ export default [
         method: 'post',
         response(option: any) {
             const { name, status, id } = option.body
-            let cateItem = cate.find(item => item.id === id)
-            if (cateItem) {
-                cateItem.name = name
-                cateItem.status = status
+            let goodsItem = cate.find(item => item.id === id)
+            if (goodsItem) {
+                goodsItem.name = name
+                goodsItem.status = status
             }
 
             return {
@@ -133,6 +178,7 @@ export default [
             }
         }
     },
+    //分类查询
     {
         url: "/querycate.json",
         method: 'post',
@@ -147,5 +193,70 @@ export default [
 
             return useArr
         }
-    }
+    },
+    //商品管理
+    {
+        url: "/getgoods.json",
+        method: 'get',
+        response(option: any) {
+            const { page, pageSize } = option.query
+            const totalPage = Math.ceil(goods.length / pageSize)
+            return {
+                result: pageGoods(page, pageSize),
+                total: goods.length,
+                totalPage
+            }
+        }
+    },
+    {
+        url: "/addgoods.json",
+        method: 'post',
+        response(option: any) {
+            const { name, cate } = option.body
+            let have = goods.find(item => item.name === name)
+            if (!have) {
+                goods.unshift({
+                    id: Mock.mock('@id()'),
+                    name,
+                    cate,
+                    setTime: Random.now('second'),
+                })
+            }
+
+            return {
+                status: 200,
+                message: !have ? '添加成功' : '不能添加重复数据'
+            }
+        }
+    },
+    {
+        url: "/editgoods.json",
+        method: 'post',
+        response(option: any) {
+            const { name, cate, id } = option.body
+            let goodsItem = goods.find(item => item.id === id)
+            if (goodsItem) {
+                goodsItem.name = name
+                goodsItem.cate = cate
+            }
+
+            return {
+                status: 200,
+                message: '编辑成功'
+            }
+        }
+    },
+    {
+        url: "/deletegoods.json",
+        method: 'get',
+        response(option: any) {
+            const { ids } = option.query
+            let id = ids.split(',')
+            goods = goods.filter(item => !id.includes(item.id))
+            return {
+                status: 200,
+                message: '删除成功'
+            }
+        }
+    },
 ]
--- packages/renderer/src/views/manage/Goods.vue
+++ packages/renderer/src/views/manage/Goods.vue
@@ -1,6 +1,96 @@
 <template>
-    <div class="cate-query">
+    <div class="cate">
         <h2>商品管理</h2>
+
+        <div class="multiple" style="margin-bottom:30px">
+            <el-button @click="handleAdd" style="width:60px">添加
+            </el-button>
+            <el-button @click="delTipsShow = true" style="width:60px" type="danger" :disabled="curItem.length === 0">
+                批量删除
+            </el-button>
+        </div>
+
+        <div class="cate-content">
+            <el-table :data="tableData" stripe highlight-current-row @selection-change="selectionChange">
+                <el-table-column type="selection" width="55" />
+
+                <el-table-column label="编号">
+                    <template #default="scope">
+                        <div style="display: flex; align-items: center">
+                            <span>{{ scope.row.id }}</span>
+                        </div>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="商品名称">
+                    <template #default="scope">
+                        <el-tag type="success" size="large">{{ scope.row.name }}</el-tag>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="所属分类">
+                    <template #default="scope">
+                        <div style="display: flex; align-items: center">
+                            <span>{{ scope.row.cate.name }}</span>
+                        </div>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="创建时间">
+                    <template #default="scope">
+                        <div style="display: flex; align-items: center">
+                            <span>{{ scope.row.setTime }}</span>
+                        </div>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button size="large" type="primary" @click="handleEdit(scope.$index, scope.row)"
+                            style="width:50px">编辑
+                        </el-button>
+                        <el-button size="large" @click="delTipsShow = true; curItem = scope.row" style="width:50px"
+                            type="danger">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="page">
+                <el-pagination background layout="prev, pager, next" :page-size="pageInfo.pageSize"
+                    :total="pageInfo.total" @current-change="pageChange" />
+            </div>
+        </div>
+
+        <el-dialog v-model="editShow" :title="formTitle" width="600px" ref="dialogForm">
+            <el-form :model="form">
+                <el-form-item label="商品名称" :label-width="140">
+                    <el-input v-model="form.name" />
+                </el-form-item>
+                <el-form-item label="所属分类" :label-width="140" style="margin-top: 30px;">
+                    <el-select v-model="form.cate" placeholder="选择商品所属分类">
+                        <el-option v-for="item in cateData" :label="item.name" :value="item.id" />
+                    </el-select>
+                </el-form-item>
+            </el-form>
+
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button size="large" @click="editShow = false" style="width:50px">取消</el-button>
+                    <el-button size="large" type="primary" @click="editConfirm" style="width:50px">确定
+                    </el-button>
+                </span>
+            </template>
+        </el-dialog>
+
+        <el-dialog v-model="delTipsShow" title="提示" width="30%">
+            <span>确定删除【{{ deltitle }}】吗？</span>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="delTipsShow = false">取消</el-button>
+                    <el-button type="primary" @click="handleDel">确定</el-button>
+                </span>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
@@ -14,17 +104,160 @@ export default {
 import { ref, reactive, computed } from 'vue'
 import axios from '../../http';
 import { ElMessage, FormInstance, FormRules } from 'element-plus'
+
+const delTipsShow = ref(false)
+
+const dialogForm = ref<FormInstance | null>()
+
+const handleDel = () => {
+    delTipsShow.value = true
+    let ids = ''
+    if (curItem.value.length) {
+        curItem.value.forEach((item: any, index: number) => {
+            if (index === 0) {
+                ids += item.id
+            } else {
+                ids += ',' + item.id
+            }
+        })
+    } else {
+        ids = (curItem.value as any).id
+    }
+
+    axios.get('/deletegoods.json', { params: { ids } }).then(res => {
+        if (res.data.status === 200) {
+            delTipsShow.value = false
+            getGoods()
+            ElMessage({
+                type: 'success',
+                message: res.data.message
+            })
+        }
+    })
+}
+
+const deltitle = computed(() => {
+    let str: string = ''
+    if (curItem.value.length) {
+        curItem.value.forEach((item: any, index: number) => {
+            if (index === 0) {
+                str += item.name
+            } else {
+                str += ',' + item.name
+            }
+        })
+    } else {
+        str = (curItem.value as any).name
+    }
+    return str
+})
+
+const formTitle = ref('添加商品')
+
+const handleAdd = () => {
+    form.value = { name: '', cate: '' }
+    editShow.value = true
+    formTitle.value = '添加商品'
+}
+
+const tableData = ref([])
+const cateData = ref<any>([])
+
+const pageInfo = reactive({
+    page: 1,
+    pageSize: 5,
+    totalPage: 0,
+    total: 0
+})
+
+const pageChange = (val: any) => {
+    pageInfo.page = val
+    getGoods()
+}
+
+const getGoods = () => {
+    axios.get('/getgoods.json', { params: { page: pageInfo.page, pageSize: pageInfo.pageSize } }).then(res => {
+        tableData.value = res.data.result.map((item: any) => {
+            return {
+                ...item,
+                cate: cateData.value.find((it: any) => it.id === item.cate)
+            }
+        })
+        pageInfo.totalPage = res.data.totalPage
+        pageInfo.total = res.data.total
+        console.log(tableData.value);
+
+    })
+}
+axios.get('/getcate.json', { params: { page: 1, pageSize: 1000 } }).then(res => {
+    cateData.value = res.data.result
+    getGoods()
+})
+
+const editShow = ref(false)
+
+const form = ref({
+    name: '',
+    cate: '',
+})
+
+const handleEdit = (index: number, row: any) => {
+    editShow.value = true
+    curItem.value = row
+    formTitle.value = '编辑商品'
+    form.value = { ...row }
+    form.value.cate = row.cate.id
+}
+
+const curItem = ref([])
+
+const selectionChange = (val: any) => {
+    curItem.value = val
+}
+
+const editConfirm = () => {
+    let url: string = ''
+    if (formTitle.value === '添加商品') {
+        url = '/addgoods.json'
+    } else {
+        url = "/editgoods.json"
+    }
+
+    axios.post(url, { ...form.value, id: (curItem.value as any).id }).then(res => {
+        if (res.data.status === 200) {
+            editShow.value = false
+            getGoods()
+            ElMessage({
+                type: 'success',
+                message: res.data.message
+            })
+        }
+    })
+}
 </script>
 
 
 <style lang="less" scoped>
-.cate-query {
+.cate {
     width: 100%;
     padding: 50px;
     padding-bottom: 0;
 
     h2 {
         .title();
+    }
+}
+
+.cate-content {
+    .el-table {
+        height: 350px;
+    }
+
+    .page {
+        padding-right: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
     }
 }
 </style>
