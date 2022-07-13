1bbb9dd01369b46816389518f5e296f5d0b2ddde
liutao <lessthan4408@163.com>
2022-07-13 15:51:12 +0800

    lt

--- packages/mock/source/modules/role.ts
+++ packages/mock/source/modules/role.ts
@@ -33,6 +33,25 @@ const roleAuthOption: IPubilc = [
     { id: 4, label: '用户管理' },
 ]
 
+const user: IPubilc[] = [
+    {
+        id: Mock.mock('@id()'),
+        name: 'admin',
+        auth: 1,
+        setTime: Random.now('week'),
+    },
+    {
+        id: Mock.mock('@id()'),
+        name: 'test',
+        setTime: Random.now('week'),
+        auth: 2
+    }
+]
+const userAuthOption: IPubilc = [
+    { id: 1, label: '系统管理员' },
+    { id: 2, label: '普通管理员' }
+]
+
 export default [
     {
         url: '/getrole.json',
@@ -75,6 +94,32 @@ export default [
             role[index].auth = newAuth
 
             return role
+        }
+    },
+    {
+        url: '/getuser.json',
+        method: 'get',
+        response() {
+            return user
+        }
+    },
+    {
+        url: '/getUserAuthOption.json',
+        method: 'get',
+        response() {
+            return userAuthOption
+        }
+    },
+    {
+        url: '/editUser.json',
+        method: 'post',
+        response(option: any) {
+            let { id, auth } = option.body
+            let index: any = user.findIndex(item => item.id === id)
+
+            user[index].auth = auth
+
+            return user
         }
     }
 ]
